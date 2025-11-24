1184f03e02639528956ae8424ebf1f64b50f6171
Lucca Barboza <luccafrbz@gmail.com>
2025-11-24 20:52:39 -0300

    classe figurinha lendária criada

---
+++ Classes/FigurinhaLendariaAutografada.h
@@ -0,0 +1,18 @@
+#ifndef FIGURINHA_LENDARIA_AUTOGRAFADA_H
+#define FIGURINHA_LENDARIA_AUTOGRAFADA_H
+
+#include "FigurinhaEspecial.h"
+
+class FigurinhaLendariaAutografada
+{
+private:
+    string autografo;
+
+public:
+    FigurinhaLendariaAutografada(/* args */);
+    ~FigurinhaLendariaAutografada();
+
+    void mostrarDetalhes();
+};
+
+#endif
